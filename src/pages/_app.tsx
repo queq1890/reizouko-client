f233065203ff188556bb844c2a1b2aa85a3119ec
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-05-04 10:47:02 +0700

    Can login with auth0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import 'tailwindcss/tailwind.css';
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 };
 
 export default MyApp;
