fb30516776a2186888f0d19c9b9702ff1936edfd
Yuji Matsumoto <yuji.matsumoto@sun-asterisk.com>
2021-05-14 08:27:46 +0700

    Fetch fridge info from user query (#6)

--- src/hooks/users/__generated__/getUser.ts
+++ src/hooks/users/__generated__/getUser.ts
@@ -7,9 +7,39 @@
 // GraphQL query operation: getUser
 // ====================================================
 
+export interface getUser_user_fridge_foods_kinds_kind {
+  name: string;
+}
+
+export interface getUser_user_fridge_foods_kinds {
+  kind: getUser_user_fridge_foods_kinds_kind;
+}
+
+export interface getUser_user_fridge_foods_categories_category {
+  name: string;
+}
+
+export interface getUser_user_fridge_foods_categories {
+  category: getUser_user_fridge_foods_categories_category;
+}
+
+export interface getUser_user_fridge_foods {
+  id: string;
+  expirationDate: any | null;
+  purchaseDate: any;
+  kinds: getUser_user_fridge_foods_kinds[];
+  categories: getUser_user_fridge_foods_categories[];
+}
+
+export interface getUser_user_fridge {
+  id: string;
+  foods: getUser_user_fridge_foods[];
+}
+
 export interface getUser_user {
   id: string;
   auth0UserId: string;
+  fridge: getUser_user_fridge | null;
 }
 
 export interface getUser {
